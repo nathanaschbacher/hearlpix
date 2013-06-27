964d5b8d060d95218bb9089fdb2cb9b778500ff8
Nathan Aschbacher <nathan.aschbacher@gmail.com>
2013-06-27 13:38:07 -0700

    Added license to source files

--- src/hearlpix.erl
+++ src/hearlpix.erl
@@ -1,3 +1,26 @@
+% (The MIT License)
+
+% Copyright (c) 2013 Nathan Aschbacher
+
+% Permission is hereby granted, free of charge, to any person obtaining
+% a copy of this software and associated documentation files (the
+% 'Software'), to deal in the Software without restriction, including
+% without limitation the rights to use, copy, modify, merge, publish,
+% distribute, sublicense, and/or sell copies of the Software, and to
+% permit persons to whom the Software is furnished to do so, subject to
+% the following conditions:
+
+% The above copyright notice and this permission notice shall be
+% included in all copies or substantial portions of the Software.
+
+% THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 -module(hearlpix).
 -author('Nathan Aschbacher <nathan@basho.com>').
 -export([new/2]).
@@ -38,7 +61,7 @@ init() ->
 -spec new(Nside::integer(), Scheme::atom()) -> hearlpix_base() | error().
 new(Nside, Scheme) ->
     erlang:nif_error({error, not_loaded}).
-    
+
 
 
 -spec ring2z(Base::hearlpix_base(), Ring::integer()) -> float() | error().
